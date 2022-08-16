d26dad5797f16765acd161852d524da9f5660f22
PedroLimam <pedrolimir@gmail.com>
2022-08-16 14:57:08 -0300

    ajuste de design e utilização de funções (mixins) no scss

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,13 @@ import {FaPlus, FaWindowClose, FaEdit} from 'react-icons/fa'
 function Main(){
 
     const [tarefas, setTarefas] = useState([])
-    const newTarefa = useRef()
+    const newTarefa = useRef('')
 
     function adcTarefas(){
+        if(typeof newTarefa.current.value[0] !== 'string') return
 
         for(let tarefa in tarefas){
-            if(tarefas[tarefa] === newTarefa.current.value){
-              return
-            }
+            if(tarefas[tarefa] === newTarefa.current.value) return      
         }
 
         setTarefas(prevState => [...prevState,  newTarefa.current.value])
@@ -23,19 +22,23 @@ function Main(){
         setTarefas(tarefasFilter)
     }
 
+    function editTarefa(el, index){
+        newTarefa.current.value = tarefas[index]
+    }
+
     return(
         <div className="container__main" >
             <h2> Lista de tarefas</h2>
 
             <form action="#">
-                <input className="container__main-input-text" ref={newTarefa} type="text" ></input>
+                <input className="container__main-input-text" ref={newTarefa} type="text" required></input>
                 <button className="container__main-button" onClick={adcTarefas}> <FaPlus/> </button>
             </form>
 
             <div className="container__main-box-tarefas" >
                 <ul>
                     { tarefas.map((el, index) => (
-                        <li className="container__main-tarefa" key={index}> {el}  <div><FaEdit className="edit"/> <FaWindowClose onClick={(e) => deleteTarefa(e, index)} className="delete" /></div></li>
+                        <li className="container__main-tarefa" key={index}> {el}  <div><FaEdit onClick={el => editTarefa(el, index)} className="edit"/> <FaWindowClose onClick={(e) => deleteTarefa(e, index)} className="delete" /></div></li>
                     )) }
                 </ul>
             </div>
